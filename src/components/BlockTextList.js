6d358a5579014a4f3efd4612432b35ba764ff6d1
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-11 17:13:13 -0300

    Creating Button and BlockTextList components to add blocks by button click

---
+++ src/components/BlockTextList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import RichText from "../components/blocks/RichTextBlock.js"
+
+const BlockTextList = () => { 
+  return (
+    <RichText/>
+  )
+}
+
+export { BlockTextList }
