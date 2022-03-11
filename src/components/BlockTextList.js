8a4be797f3d3616f3139e5e921f0f4d604763af0
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-11 18:48:25 -0300

    Using "react-dnd" dependency to drag n drop functionality

--- src/components/BlockTextList.js
+++ src/components/BlockTextList.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 import RichText from "../components/blocks/RichTextBlock.js"
 
-const BlockTextList = () => { 
+//import { useDrag } from "react-dnd"
+
+const BlockTextList = () => {
+  /*
+  const [{ isDragging }, drag] = useDrag(() => (
+    {
+      type: "textBlock",
+      collect: (monitor) => ({
+        isDragging: !!monitor.isDragging(),
+      }),
+    }
+  ))
+  */
+
   return (
-    <RichText/>
+    <RichText /*ref={drag} style={{ border: isDragging ? "4px solid white" : "0px" 8 }}*//>
   )
 }
 
