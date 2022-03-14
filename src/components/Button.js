e7678e361b9b65b57541445ae433caa04555c3e1
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-14 18:50:16 -0300

    Changing Button text

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => { 
   return ( 
-    <button onClick={props.onClick}>Add New Block Text</button> 
+    <button onClick={props.onClick}>Add New Text Block</button> 
   )
 } 
 
